55d51da11a2a4cba9ad787c1448cbaa06ad5f9f7
Adeilton Bolzani <ti08@biopele.com.br>
2022-12-07 10:48:50 -0300

    PointTwo

--- src/Pages/Start/Index.jsx
+++ src/Pages/Start/Index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { View, ScrollView, Text, Image } from "react-native";
+import { View, StyleSheet, ScrollView, Text, Image } from "react-native";
+
+import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start(){
     return (
-        <View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
-                    <Image source={require("../../assets/icons/logo3.png")}></Image>
-                    <Text>
+        <View style={styles.container}>
+            <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+                <View style={{ alignItems: "center" }}>
+                    <Image source={require("../../assets/icons/logo3.png")} style={styles.logo}></Image>
+                    <LifeStatus></LifeStatus>
+                    <Text style={styles.description}>
                         Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível.
                     </Text>
                 </View>
@@ -15,3 +18,22 @@ export default function Start(){
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    logo: {
+      width: 300,
+      height: 60,
+      marginTop: 60,
+      marginBottom: 20,
+    },
+    description: {
+      color: "#FFFFFF",
+      fontSize: 20,
+      textAlign: "center",
+      marginVertical: 60,
+    },
+  });
